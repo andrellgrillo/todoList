5fedf19a84721e63af632793934fd8c83f268199
Andre Grillo <andrell.grillo@gmail.com>
2022-07-05 17:29:07 -0300

    taskList - iniciado

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -5,7 +5,7 @@ import styles from './TaskList.module.css';
 export function TaskList() {
   return (
     <div className={styles.taskList}>
-      <div className="Empty">
+      <div className={styles.empty}>
         <ListBullets size={56} />
         <p className={styles.pHeader}>Você ainda não te tarefas cadastradas</p>
         <p className={styles.pSubHeader}>Crie tarefas e organize seus itens a fazer</p>
