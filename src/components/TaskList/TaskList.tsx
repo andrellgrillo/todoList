91b1de08f3fb46c6db9f153ecda85a5f5ef9c680
Andre Grillo <andrell.grillo@gmail.com>
2022-07-06 07:44:37 -0300

    alteração TaskList

--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -6,9 +6,13 @@ export function TaskList() {
   return (
     <div className={styles.taskList}>
       <div className={styles.empty}>
-        <ListBullets size={56} />
-        <p className={styles.pHeader}>Você ainda não te tarefas cadastradas</p>
-        <p className={styles.pSubHeader}>Crie tarefas e organize seus itens a fazer</p>
+        <div className={styles.icon}> 
+          <ListBullets size={56} />
+        </div>
+        <div>
+          <p className={styles.pHeader}>Você ainda não tem tarefas cadastradas</p>
+          <p className={styles.pSubHeader}>Crie tarefas e organize seus itens a fazer</p>
+        </div>
       </div>
     </div>
   )
